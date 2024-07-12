57e39f78063f5c506fba4cd0a822618e4023069f
yapping <deviljin515@gmail.com>
2024-07-13 02:43:36 +0800

    Commit 2

--- main.py
+++ main.py
@@ -43,6 +43,18 @@ print("-- Please choose a drink to buy: --\n")
 
 total = 0
 stillBuying = True
+def totalCalc(addedDrinks):
+   return sum(drink['price'] for drink in addedDrinks)
+def paymentProcess(total):
+   notes = float(input(f"\nYour total is ${total}. Please enter the amount in notes: "))
+   if notes >= total:
+      change = notes - total
+      if change > 0:
+         print(f"Thank you! Your change is: ${change:.2f}")
+      else:
+         print("Thank you! No change is needed.")
+   else:
+      print("Insufficient amount! Please enter a higher value.")
 
 for i in drinks:
    print(f"Drink: {i['drink']} --- Price: {i['price']} --- ID: {i['id']}")
@@ -55,8 +67,10 @@ def vendingMachine(drinks, stillBuying, addedDrinks):
         print("Drink has been added")
 
    if not stillBuying:
-      total_sum = sum(item.get('price', item.get('itemPrice')) for item in addedDrinks)
-      print(f"\nTotal sum of selected items: ${total_sum}")
+      if addedDrinks:
+         sumOfNotes = totalCalc(addedDrinks)
+         paymentProcess(sumOfNotes)
+
 def sumItem(item):
    sumItems = 0
    for i in item:
